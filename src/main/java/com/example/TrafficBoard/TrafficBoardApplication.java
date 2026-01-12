88ee48b377cc35fa93e602d8c5d8282ebf664ebd
postwo <codo7717@naver.com>
2026-01-12 23:54:44 +0900

    세팅

---
+++ src/main/java/com/example/TrafficBoard/TrafficBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrafficBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficBoardApplication.class, args);
+	}
+
+}
