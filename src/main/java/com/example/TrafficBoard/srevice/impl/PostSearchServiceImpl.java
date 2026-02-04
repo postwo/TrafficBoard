005e34f6fc86ab1a00b19ca69d56b218b6dcb5b6
postwo <codo7717@naver.com>
2026-02-04 20:31:41 +0900

    비동기 설정과 redis timeout 시간 수정

--- src/main/java/com/example/TrafficBoard/srevice/impl/PostSearchServiceImpl.java
+++ src/main/java/com/example/TrafficBoard/srevice/impl/PostSearchServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.TrafficBoard.srevice.PostSearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,6 +25,7 @@ public class PostSearchServiceImpl implements PostSearchService {
     //이유: 메인 로직은 @Async 메서드가 끝나기도 전에 "나 다음 줄 실행할래!" 하고 넘어가 버리는데, 아직 데이터(List)는 도착하지 않았기 때문
     //결과: 데이터가 비어 있거나 null인 상태로 화면이 그려질 위험
     //결론: @Async는 주로 "결과를 즉시 돌려받지 않아도 되는 작업"(로그 기록, 메일 발송, 푸시 알람 등)에 주로 사용
+    @Async // 이거는 일단 테스트 해보기 위해 설정 
     @Override
     // Cacheable은 메서드를 실행하기전에 먼저 key를 가진 데이터가 캐시에 있나 확인후 메서드를 실행
     // 캐시에 데이터가 없으면 메서드를 실행후 데이터를 캐시에 저장
