9018d5d4cb7d8bf04cdde618da1834346565546f
postwo <codo7717@naver.com>
2026-02-10 22:28:18 +0900

    aws 알람 config

---
+++ src/main/java/com/example/TrafficBoard/config/AWSConfig.java
@@ -0,0 +1,22 @@
+package com.example.TrafficBoard.config;
+
+import lombok.Getter;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class AWSConfig {
+
+    @Value("${sns.topic.arn}")
+    private String snsTopicARN;
+
+    @Value("${aws.accessKey}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+}
